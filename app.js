637522bbb1d5e09f2af247b8c7742f267e7be29e
Christelle LEGROS <christelle.legros80@sfr.fr>
2022-03-04 17:12:20 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const app = express();
+const nodemailer = require("nodemailer");
+const cors = require("cors");
+require("dotenv").config();
+
+const port = process.env.PORT || 8000;
+app.use(express.json());
+
+const corsOptions = {
+  origin: "localhost:3000",
+  credentials: true,
+  optionSuccessStatus: 200,
+  maxAge: 3600,
+};
+app.use(cors(corsOptions));
+
+app.post("/", (req, res) => {
+  const { name, subject, email, message } = req.body;
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: "christelle.legros80@gmail.com",
+    to: process.env.EMAIL,
+    subject: subject,
+    text: `Name: ${name}, email: ${email}
+    Message: ${message}`,
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
+
+  res.status(200).send();
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
